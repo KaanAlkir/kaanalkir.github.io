dabfda08b661bdf86f1bc944d02735ccd0dda95d
KaanAlkir <136902567+KaanAlkir@users.noreply.github.com>
2025-06-21 18:59:32 +0000

    Deploying to gh-pages from @ KaanAlkir/kaanalkir.github.io@9f74539c14c89e158a2a651884612fb43c4eae1c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "Projects",
+          title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
@@ -38,7 +38,7 @@ ninja.data = [{
             window.location.href = "/talks/";
           },
         },{id: "nav-cv",
-          title: "CV",
+          title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
