156f321a635d1efea47066f5b63cce5951741567
KaanAlkir <136902567+KaanAlkir@users.noreply.github.com>
2025-06-23 21:02:55 +0000

    Deploying to gh-pages from @ KaanAlkir/kaanalkir.github.io@dbb39239ffc0d0b6ecdfc363405e973369eb8a06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-interactive-gas-simulation-of-navier-stokes-driven-dynamical-systems-in-2-d",
+          title: 'Interactive Gas Simulation of Navier–Stokes-Driven Dynamical Systems in 2-D',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
