708255c06036f99af08cdcc1959e185630f9bc8e
KaanAlkir <136902567+KaanAlkir@users.noreply.github.com>
2025-06-23 21:11:11 +0000

    Deploying to gh-pages from @ KaanAlkir/kaanalkir.github.io@b0afa60e7f95fe1d071024ad69361599b5047e9c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -432,7 +432,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-interactive-gas-simulation-of-navier-stokes-driven-dynamical-systems-in-2-d",
           title: 'Interactive Gas Simulation of Navier–Stokes-Driven Dynamical Systems in 2-D',
-          description: "with background image",
+          description: "This project is an interactive simulation of a 2D dynamical system coupled with a fluid flow based on the Navier–Stokes equations. Users can adjust the wind field type and parameters, including custom vector fields, to see how they affect the motion of particles and scalar density. It’s designed to help visualize how external flows influence dynamical behavior in a simple and intuitive way.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
