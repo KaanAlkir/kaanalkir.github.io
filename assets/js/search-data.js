0c28e2c232777613bb03dbabf2d0a1b8e9a6b31c
KaanAlkir <136902567+KaanAlkir@users.noreply.github.com>
2025-06-27 23:33:25 +0000

    Deploying to gh-pages from @ KaanAlkir/kaanalkir.github.io@4dfb7c7d6b013a896151a05aae740b4a30f428c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-interactive-gas-simulation-of-navier-stokes-driven-dynamical-systems-in-2-d",
-          title: 'Interactive Gas Simulation of Navier–Stokes-Driven Dynamical Systems in 2-D',
+          section: "News",},{id: "projects-interactive-fluid-simulation-of-navier-stokes-driven-dynamical-systems-in-2-d",
+          title: 'Interactive Fluid Simulation of Navier–Stokes-Driven Dynamical Systems in 2-D',
           description: "This project is an interactive simulation of a 2D dynamical system coupled with a fluid flow based on the Navier–Stokes equations. Users can adjust the wind field type and parameters, including custom vector fields, to see how they affect the motion of particles and scalar density. It’s designed to help visualize how external flows influence dynamical behavior in a simple and intuitive way.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
